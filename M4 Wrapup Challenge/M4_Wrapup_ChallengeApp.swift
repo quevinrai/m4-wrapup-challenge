a515f12f212d2c77eb05905130825cd7601efdaa
Quevin Bambasi <quevinbambasi@pm.me>
2023-03-22 16:02:01 -0400

    Initial Commit

---
+++ M4 Wrapup Challenge/M4_Wrapup_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4_Wrapup_ChallengeApp.swift
+//  M4 Wrapup Challenge
+//
+//  Created by Quevin Bambasi on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct M4_Wrapup_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
